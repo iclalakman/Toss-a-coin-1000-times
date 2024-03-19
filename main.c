d9732a3b7116f4032f9770ff4c079e2e6e8fb478
iclalakman <163682519+iclalakman@users.noreply.github.com>
2024-03-19 23:02:45 +0200

    Add files via upload
    Toss a coin 1000 times.

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#define HEAD 1
+int main() {
+    int toss,i;
+    int numberofhead=0;
+
+    srand(time(NULL));
+    printf("You tossed the coin 1000 times\n");
+
+    for (i=0 ;i<1000 ;i++ ) {
+        toss = (int) (rand() % 2);
+
+        if (toss == HEAD){
+            numberofhead+=1;}
+    }
+
+    printf("Number of heads: %d \n",numberofhead);
+    printf("The number of tails: %d",1000-numberofhead);
+    return 0;
+}
+
+
+
+
